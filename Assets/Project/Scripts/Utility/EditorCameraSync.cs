86336bcd9f785344677571519fd1e23a84cfdd12
nkc20236039 <nkc23.kawai.yuto@gmail.com>
2024-11-14 00:56:14 +0900

    Fix: シーンビューがないときにDOTSカメラを起動しないように修正

--- Assets/Project/Scripts/Utility/EditorCameraSync.cs
+++ Assets/Project/Scripts/Utility/EditorCameraSync.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -17,8 +18,30 @@ public class EditorCameraSync : MonoBehaviour
 
     void Update()
     {
+        // シーンビューが無ければ実行しない
+        if(SceneView.lastActiveSceneView == null) { return; }
+
+        if (SceneView.lastActiveSceneView.hasFocus)
+        {
+            // シーンビューにフォーカスされていればゲームビューのDisplayをカメラのDiplayに変える
+            EditorWindow gameView = EditorWindow.GetWindow(GetTypeFromName("UnityEditor.GameView"));
+            var displayField = gameView.GetType().GetField("m_TargetDisplay", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            if (displayField != null)
+            {
+                displayField.SetValue(gameView, gameCamera.targetDisplay);
+                gameView.Repaint();  // Display変更後にビューを更新
+            }
+        }
+
         gameCamera.transform.position = sceneCamera.transform.position;
         gameCamera.transform.rotation = sceneCamera.transform.rotation;
         gameCamera.fieldOfView = sceneCamera.fieldOfView;
+    }
+
+    private static System.Type GetTypeFromName(string name)
+    {
+        return System.AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
+            .FirstOrDefault(type => type.FullName == name);
     }
 }
