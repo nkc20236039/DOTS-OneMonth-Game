47a3937832b5f146965bdd99169f4a48fefe3915
nkc20236039 <nkc23.kawai.yuto@gmail.com>
2024-12-02 00:49:16 +0900

    Update: レベルが上がるとUIを表示するように更新

---
+++ Unlimited-Surviver/Assets/Project/Scripts/DOTS/System/Event/EnhancementUISystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Unity.Entities;
+
+namespace DOTS
+{
+    public partial class EnhancementUISystem : SystemBase
+    {
+        private EnhancementUIManagedSingleton completedEvent;
+
+        protected override void OnCreate()
+        {
+            EntityManager.CreateSingleton(new EnhancementUIManagedSingleton());
+            completedEvent = SystemAPI.ManagedAPI.GetSingleton<EnhancementUIManagedSingleton>();
+
+            RequireForUpdate<EnhancementUIManagedSingleton>();
+            RequireForUpdate<LevelSingleton>();
+        }
+
+        protected override void OnUpdate()
+        {
+            foreach (var level in SystemAPI.Query<RefRW<LevelSingleton>>())
+            {
+                if (level.ValueRO.IsLevelUp)
+                {
+                    completedEvent.Show?.Invoke();
+                    level.ValueRW.IsLevelUp = false;
+                }
+            }
+        }
+    }
+}
---
+++ Unlimited-Surviver/Assets/Project/Scripts/Mono/UI/View/EnhancementPView.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Mono
+{
+    public class EnhancementPView : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject canvas;
+
+        public void Show()
+        {
+            canvas.SetActive(true);
+        }
+    }
+}
